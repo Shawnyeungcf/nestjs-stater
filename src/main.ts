ca4d3310f55574b9485e0a92ae452f24996c8ad8
Shawn Yeung <shawnyeungcf@gmail.com>
2022-08-14 18:52:11 +0800

    feat: add log

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+import { AppModule } from './modules/app/app.module';
+import { Logger } from 'nestjs-pino';
+
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  app.useLogger(app.get(Logger));
   await app.listen(3000);
 }
 bootstrap();
